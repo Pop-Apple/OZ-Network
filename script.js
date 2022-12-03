d6685971b8dd769f3bfda4dffbc2d85cded48216
Pop-Apple <101918076+Pop-Apple@users.noreply.github.com>
2022-12-03 22:11:21 +0900

    Correction of areas of concern

---
+++ script.js
@@ -0,0 +1,9 @@
+const TextUserName = document.getElementById('TextUserName');
+const TextPassword = document.getElementById('TextPassword');
+const LoginButton = document.getElementById('LoginButton');
+
+const functionButton = () => {
+    if(!TextUserName.value.trim()) {
+    } else {
+    }
+  };
